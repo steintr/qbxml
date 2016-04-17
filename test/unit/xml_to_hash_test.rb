403f4e14b95d25774bc52b7d8784c2b3c3ae16a9
Russell Steinthal <rms39@columbia.edu>
2016-04-17 12:39:20 -0400

    Add a failing test for a response with an empty element

--- test/unit/xml_to_hash_test.rb
+++ test/unit/xml_to_hash_test.rb
@@ -26,5 +26,11 @@ class XmlToHashTest < Minitest::Test
     assert_equal h, qbxml.from_qbxml("<?qbxml version=\"7.0\"?>\n<QBXML>\n  <QBXMLMsgsRq>\n    <InvoiceQueryRq>\n      <IncludeRetElement>TxnID</IncludeRetElement>\n    <IncludeRetElement>RefNumber</IncludeRetElement>\n    </InvoiceQueryRq>\n  </QBXMLMsgsRq>\n</QBXML>\n")
   end
 
+  def test_empty_response
+    response_str = "<?xml version=\"1.0\"?>\r<?qbxml version=\"7.0\"?>\r<QBXML>\r<QBXMLMsgsRs />\r</QBXML>\r"
+    qbxml = Qbxml.new
+    h = { "qbxml" => { "xml_attributes" => {}, "qbxml_msgs_rs" => {} } }
+    assert_equal h, qbxml.from_qbxml(response_str)
+  end
 end
 
